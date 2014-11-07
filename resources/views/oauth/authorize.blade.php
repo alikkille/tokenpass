00143fc68defbe2f68a3dd47494d0f915d65079b
Devon Weller <dweller@devonweller.com>
2014-11-06 20:36:33 -0600

    visual updates to grant authorization template

--- resources/views/oauth/authorize.blade.php
+++ resources/views/oauth/authorize.blade.php
@@ -7,6 +7,15 @@
 
 @section('bodyContent')
 
+            <p>The application <span class="clientName">{{{ $client->getName() }}}</span> has requested the following privileges:</p>
+            <ul>
+            @foreach ($scopes as $scope)
+            <li>{{{ $scope->getDescription() }}} </li>
+            @endforeach
+            </ul>
+
+            <p>What would you like to do?</p>
+
             @include('partials.errors')
 
             {!! Form::open(['route' => 'oauth.authorize', 'method' => 'post', 'url' => Request::fullUrl()]) !!}
