68e85f2ae3b944bbe3be2ea0c593662aaedecdfa
Devon Weller <dweller@devonweller.com>
2014-11-13 09:18:43 -0600

    added clear opcache function

---
+++ public/clear_opcache.php
@@ -0,0 +1,15 @@
+<?php 
+
+header('content-type: application/json');
+
+if (!strlen($_GET['k']) OR $_GET['k'] != getenv('OPCACHE_KEY')) {
+    http_response_code(401);
+    echo json_encode(['success' => false, 'error' => 'invalid key', 'timestamp' => time()], 192);
+    exit();
+}
+
+
+opcache_reset();
+
+echo json_encode(['success' => true, 'timestamp' => time()], 192);
+
