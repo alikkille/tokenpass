a5e3184eb52d74839c4ca4e99a9470ff5c855c01
Devon Weller <devon@tokenly.co>
2015-08-20 16:35:06 -0500

    fix deploy to build public assets properly.

--- .deploy.sh
+++ .deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-/usr/local/bin/composer.phar install
-./artisan migrate
+set -e
+
+echo; echo "updating composer dependencies"
+/usr/local/bin/composer.phar install --prefer-dist --no-progress
+
+echo; echo "copying sample environment"
+cp -v .env.example .env
+
+echo; echo "compiling assets"
+DIR=`pwd`
+cd resources/assets;
+npm install;
+./node_modules/gulp/bin/gulp.js
+cd $DIR
