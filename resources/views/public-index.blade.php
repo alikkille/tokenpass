2b8208d6b9a647876872c647c4781dfb390691bb
Devon Weller <devon@tokenly.co>
2015-08-20 21:38:15 -0500

    Home page tweaks.

--- resources/views/public-index.blade.php
+++ resources/views/public-index.blade.php
@@ -1,16 +1,25 @@
 @extends('layouts.guest')
 
+
 @section('body_content')
 
 <h1>Welcome to Tokenly Accounts</h1>
 
+<div class="spacer1"></div>
+
 <p>Tokenly Accounts is where you register and login for Tokenly services such as Swapbot.</p>
+
+
+<div class="spacer1"></div>
+
+<p>If you have a <a href="https://letstalkbitcoin.com">Let's Talk Bitcoin</a> account with, then you can create a Tokenly Account immediately by logging in with your existing login and password.</p>
+<a href="/auth/login" class="btn btn-primary">Login</a>
+
 
 <div class="spacer2"></div>
 
-<a href="/auth/login" class="btn btn-primary">Login</a>
+<p>You can also register and create a new Tokenly Account.</p>
 
-<a href="/auth/register" class="btn btn-success">Register</a>
+<a href="/auth/register" class="btn btn-success">Create a New Account</a>
 
 @endsection
-
