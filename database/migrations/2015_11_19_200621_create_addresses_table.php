11829a646bbb23301fe397e734a2e466d969aa94
Nick Rathman <nickrathman@gmail.com>
2015-11-19 12:16:39 -0800

    added coin_addresses and address_balances tables for TCA

---
+++ database/migrations/2015_11_19_200621_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coin_addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->string('type');
+            $table->string('address');
+            $table->string('label')->nullable();
+            $table->boolean('verified')->default(0);
+            $table->boolean('public')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('coin_addresses');
+    }
+}
