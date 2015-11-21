a8fb7ea5aa02a76ffab30b66ef0349e809c127b7
Devon Weller <devon@tokenly.co>
2015-11-21 11:18:35 -0600

    Some composer and integration fixes.

--- app/Console/Commands/ScanCoinAddresses.php
+++ app/Console/Commands/ScanCoinAddresses.php
@@ -4,7 +4,6 @@ namespace TKAccounts\Console\Commands;
 
 use Illuminate\Console\Command;
 use TKAccounts\Models\Address;
-use Tokenly\XChainClient\Client;
 
 class ScanCoinAddresses extends Command
 {
@@ -23,30 +22,22 @@ class ScanCoinAddresses extends Command
     protected $description = 'Looks at all registered cryptocurrency addresses and contacts XChain to update the local balance cache';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->xchain = new Client(env('XCHAIN_CONNECTION_URL'), env('XCHAIN_API_TOKEN'), env('XCHAIN_API_KEY'));
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
+        // The XChainClient service provider will build the client and apply xchain credentials
+        $xchain = app('Tokenly\XChainClient\Client');
+
         $address_list = Address::where('verified', '=', 1)->get();
         if(!$address_list OR count($address_list) == 0){
 			return false;
 		}
 		$stamp = date('Y-m-d H:i:s');
 		foreach($address_list as $row){
-			$balances = $this->xchain->getBalances($row->address, true);
+			$balances = $xchain->getBalances($row->address, true);
 			if($balances AND count($balances) > 0){
 				$update = Address::updateAddressBalances($row->id, $balances);
 				if(!$update){
--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
         LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
         Tokenly\ConsulHealthDaemon\ServiceProvider\ConsulHealthDaemonServiceProvider::class,
+        Tokenly\XChainClient\XChainServiceProvider::class,
 
         /*
          * Application Service Providers...
