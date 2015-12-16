3cf58efe1e55b95ffedb54a853df01d6e0e5321a
Nick Rathman <nickrathman@gmail.com>
2015-12-16 15:06:12 -0800

    made TCA API methods require client API key and require user to have authenticated once with tca scope for application

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -3,6 +3,8 @@ namespace TKAccounts\Http\Controllers\API;
 use TKAccounts\Http\Controllers\Controller;
 use TKAccounts\Models\User, TKAccounts\Models\Address;
 use TKAccounts\Providers\CMSAuth\CMSAccountLoader;
+use TKAccounts\Models\OAuthClient as AuthClient;
+use TKAccounts\Models\OAuthScope as Scope;
 use DB, Exception, Response, Input;
 use Illuminate\Http\JsonResponse;
 use Tokenly\TCA\Access;
@@ -12,8 +14,23 @@ class APIController extends Controller
 
 	public function checkTokenAccess($username)
 	{
+		$input = Input::all();
 		$output = array();
 		$http_code = 200;
+		
+		//check if a valid application client_id
+		$valid_client = false;
+		if(isset($input['client_id'])){
+			$get_client = AuthClient::find(trim($input['client_id']));
+			if($get_client){
+				$valid_client = $get_client;
+			}
+		}
+		if(!$valid_client){
+			$output['error'] = 'Invalid API client ID';
+			$output['result'] = false;
+			return Response::json($output, 403);
+		}
 		
 		$getUser = User::where('username', $username)->first();
 		if(!$getUser){
@@ -36,7 +53,32 @@ class APIController extends Controller
 			}
 		}
 		else{
-			$input = Input::all();
+			
+			//make sure user has authenticated with this application at least once
+			$find_connect = DB::table('client_connections')->where('user_id', $getUser->id)->where('client_id', $valid_client->id)->first();
+			if(!$find_connect OR count($find_connect) == 0){
+				$output['error'] = 'User has not authenticated yet with client application';
+				$output['result'] = false;
+				return Response::json($output, 403);
+			}
+			
+			//look for the TCA scope
+			$get_scope = Scope::find('tca');
+			if(!$get_scope){
+				$output['error'] = 'TCA scope not found in system';
+				$output['result'] = false;
+				return Response::json($output, 500);
+			}
+			
+			
+			//make sure scope is applied to client connection
+			$scope_connect = DB::table('client_connection_scopes')->where('connection_id', $find_connect->id)->where('scope_id', $get_scope->uuid)->get();
+			if(!$scope_connect OR count($scope_connect) == 0){
+				$output['error'] = 'User does not have TCA scope applied for this client application';
+				$output['result'] = false;
+				return Response::json($output, 403);
+			}
+			
 			$ops = array();
 			$stack_ops = array();
 			$checks = array();
@@ -84,12 +126,52 @@ class APIController extends Controller
 	{
 		$output = array();
 		$http_code = 200;
+		$input = Input::all();
+		
+		//check if a valid application client_id
+		$valid_client = false;
+		if(isset($input['client_id'])){
+			$get_client = AuthClient::find(trim($input['client_id']));
+			if($get_client){
+				$valid_client = $get_client;
+			}
+		}
+		if(!$valid_client){
+			$output['error'] = 'Invalid API client ID';
+			$output['result'] = false;
+			return Response::json($output, 403);
+		}		
 		
 		$user = User::where('username', $username)->first();
 		if(!$user){
 			$http_code = 404;
 			$output['result'] = false;
 			$output['error'] = 'Username not found';
+		}
+		
+		//make sure user has authenticated with this application at least once
+		$find_connect = DB::table('client_connections')->where('user_id', $user->id)->where('client_id', $valid_client->id)->first();
+		if(!$find_connect OR count($find_connect) == 0){
+			$output['error'] = 'User has not authenticated yet with client application';
+			$output['result'] = false;
+			return Response::json($output, 403);
+		}
+		
+		//look for the TCA scope
+		$get_scope = Scope::find('tca');
+		if(!$get_scope){
+			$output['error'] = 'TCA scope not found in system';
+			$output['result'] = false;
+			return Response::json($output, 500);
+		}
+		
+		
+		//make sure scope is applied to client connection
+		$scope_connect = DB::table('client_connection_scopes')->where('connection_id', $find_connect->id)->where('scope_id', $get_scope->uuid)->get();
+		if(!$scope_connect OR count($scope_connect) == 0){
+			$output['error'] = 'User does not have TCA scope applied for this client application';
+			$output['result'] = false;
+			return Response::json($output, 403);
 		}
 		
 		$address_list = Address::getAddressList($user->id, 1);
