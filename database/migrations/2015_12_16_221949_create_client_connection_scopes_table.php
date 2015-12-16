f8a95b4f2e341698016853a816a9fca023579719
Nick Rathman <nickrathman@gmail.com>
2015-12-16 15:04:00 -0800

    added client_connection_scopes table

---
+++ database/migrations/2015_12_16_221949_create_client_connection_scopes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientConnectionScopesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_connection_scopes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('connection_id')->unsigned();
+            $table->foreign('connection_id')
+                  ->references('id')->on('client_connections')
+                  ->onDelete('cascade');
+            $table->string('scope_id');
+            $table->foreign('scope_id')
+                  ->references('uuid')->on('oauth_scopes')
+                  ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('client_connection_scopes');
+    }
+}
