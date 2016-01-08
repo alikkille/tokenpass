c35192a1e1d10eaf682a359babf21663712f056e
cryptonaut <nrathman@ironcladtech.ca>
2016-01-08 10:07:36 -0800

    updated sidebar style and links

--- resources/views/accounts/includes/sidebar.blade.php
+++ resources/views/accounts/includes/sidebar.blade.php
@@ -2,14 +2,15 @@
   <div class="panel-body">
     <h4>Tokenly Accounts</h4>
     <div class="spacer1"></div>
-    <ul class="list-unstyled">
-        <li><a href="/dashboard">Accounts Home</a></li>
-        <li><a href="/auth/update">Update my Account</a></li>
-        <li><a href="/auth/connectedapps">My Connected Applications</a></li>
-        
+    <ul class="accounts-menu">
+        <li><a href="/dashboard"><i class="fa fa-home"></i> Accounts Home</a></li>
+        <li><a href="/auth/update"><i class="fa fa-gears"></i> Update Account</a></li>
+		<li><a href="/inventory"><i class="fa fa-btc"></i> Token Inventory</a></li>
+        <li><a href="/auth/connectedapps"><i class="fa fa-globe"></i> Connected Applications</a></li>
+        <li><a href="#"><i class="fa fa-code-fork"></i> API Keys / My Apps</a></li>
         <li>
             <hr>
-            <a href="/auth/logout">Logout</a>
+            <a href="/auth/logout"><i class="fa fa-sign-out"></i> Logout</a>
         </li>
     </ul>
   </div>
