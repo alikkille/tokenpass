443ee4dac32a4b8e8e80779a8660b4e80be5aa5d
cryptonaut <nrathman@ironcladtech.ca>
2016-01-08 10:08:28 -0800

    built user meta data table

---
+++ app/Models/UserMeta.php
@@ -0,0 +1,47 @@
+<?php
+namespace TKAccounts\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class UserMeta extends Model
+{
+	protected $table = 'user_meta';
+	
+	public static function allUser($id)
+	{
+		$getAll = UserMeta::where('user_id', '=', $id)->all();
+		$output = array();
+		foreach($getAll as $row){
+			$output[$row->meta_key] = $row->meta_value;
+		}
+		return $output;
+	}
+	
+	public static function getMeta($id, $key)
+	{
+		$get = UserMeta::where('user_id', '=', $id)->where('meta_key', '=', $key)->first();
+		if(!$get){
+			return false;
+		}
+		return $get->meta_value;
+	}
+	
+	public static function setMeta($id, $key, $value, $access_level = 0, $owner_client = 0, $extra = '')
+	{
+		$get = UserMeta::where('user_id', '=', $id)->where('meta_key', '=', $key)->first();
+		if(!$get){
+			$get = new UserMeta;
+			$get->user_id = $id;
+			$get->meta_key = $key;
+			$get->access_level = $access_level;
+			$get->owner_client = $owner_client;
+			$get->extra = $extra;
+		}
+		$get->meta_value = $value;
+		$get->save();
+		return true;
+	}
+	
+	
+}
---
+++ database/migrations/2016_01_08_171221_create_user_meta_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_meta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->string('meta_key');
+            $table->index('meta_key');
+            $table->text('meta_value')->nullable();
+            $table->integer('access_level')->default(0);
+            $table->integer('owner_client')->unsigned()->default(0);
+            $table->text('extra')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_meta');
+    }
+}
