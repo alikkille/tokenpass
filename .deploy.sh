d86e8a1dddf54946b0a31de9bfc04101402e13d1
Devon Weller <devon@tokenly.co>
2016-04-07 10:16:06 -0500

    Simplified deploy script again

--- .deploy.sh
+++ .deploy.sh
@@ -11,13 +11,7 @@ cp -v .env.example .env
 echo; echo "compiling assets"
 DIR=`pwd`
 cd resources/assets;
-npm cache clean;
-# try up to 4 times
-COUNTER=0
-while [  $COUNTER -lt 4 ]; do
-    let COUNTER=COUNTER+1 
-    npm install && break
-done
+npm install
 
 
 ./node_modules/gulp/bin/gulp.js
