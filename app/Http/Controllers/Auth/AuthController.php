7dff6a49b87a9484c8dd73bcfc82b5fbbc4eee48
Devon Weller <devon@tokenly.co>
2016-06-08 07:51:20 -0500

    Do not show error for missing slug when username is blank

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -361,7 +361,7 @@ class AuthController extends Controller
         return Validator::make($data, [
             'name'     => 'required|max:255',
             'username' => 'required|max:255|unique:users',
-            'slug'     => 'required|max:255|unique:users',
+            'slug'     => 'sometimes|max:255|unique:users',
             'email'    => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
