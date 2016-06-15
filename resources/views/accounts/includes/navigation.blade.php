3e98d66e8087b0eb08f9fa31ab8f3ba76dc83e8d
Adam Raider <adamjraider@gmail.com>
2016-06-15 16:49:27 -0400

    Add api keys to dashboard navigation

---
+++ resources/views/accounts/includes/navigation.blade.php
@@ -0,0 +1,52 @@
+<div class="dash-nav-menu-mobile" id="mobile-nav">
+  <div class="landing-nav-menu-header"><i class="material-icons toggle-mobile-nav">close</i></div>
+  <ul>
+    <li><a href="/inventory">Inventory</a></li>
+    <li><a href="/pockets">Pockets</a></li>
+    <li><a href="/auth/connectedapps">Integrations</a></li>
+    <li><a href="/auth/apps">Api Keys</a></li>
+  </ul>
+  <hr>
+  <ul>
+    <li><a href="/auth/logout">Logout</a></li>
+  </ul>
+</div>
+<div class="navigation">
+  <div class="navigation-content">
+    <div class="logo">
+      <a href="/">
+        <span>token<strong>pass</strong></span>
+      </a>
+    </div>
+    <div class="user">
+      <div class="avatar">
+        <a href="/auth/update">
+          <i class="material-icons">person</i>
+        </a>
+      </div>
+      <i>
+        <a href="/auth/logout">
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="#fff" d="M17,17.25V14H10V10H17V6.75L22.25,12L17,17.25M13,2A2,2 0 0,1 15,4V8H13V4H4V20H13V16H15V20A2,2 0 0,1 13,22H4A2,2 0 0,1 2,20V4A2,2 0 0,1 4,2H13Z"></path>
+          </svg>
+        </a>
+      </i>
+    </div>
+    <div class="dash-nav-menu-btn">
+      <i class="material-icons toggle-mobile-nav">
+        dehaze
+      </i>
+    </div>
+    <ul class="menu">
+      <li><a href="/inventory">Inventory</a></li>
+      <li><a href="/pockets">Pockets</a></li>
+      <li><a href="/auth/connectedapps">Integrations</a></li>
+      <li><a href="/auth/apps">Api Keys</a></li>
+        
+      <!-- TODO -->
+      <!-- <li><a href="/dashboard"><i class="fa fa-home"></i> Accounts Home</a></li> -->
+      <!-- <li><a href="/auth/update"><i class="fa fa-gears"></i> Update Account</a></li> -->
+      <!-- <li><a href="/auth/apps"><i class="fa fa-code-fork"></i> API Keys / My Apps</a></li> -->
+    </ul>
+  </div>
+</div>
