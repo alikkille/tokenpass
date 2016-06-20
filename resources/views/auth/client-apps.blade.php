575e4bf01833ecc5c91ac6ac5fa0cdd35d5ce4f4
Adam Raider <adamjraider@gmail.com>
2016-06-20 14:13:56 -0400

    Standardize panels on client-apps and update account

--- resources/views/auth/client-apps.blade.php
+++ resources/views/auth/client-apps.blade.php
@@ -12,46 +12,47 @@
 </section>
 
 <section id="appsController">
-	@if(Session::has('message'))
-		<p class="alert {{ Session::get('message-class') }}">{{ Session::get('message') }}</p>
-	@endif	
-	
-	<p>
-		Here you can register new client Applications and obtain a pair of API keys for integration of Tokenpass
-		in your own website or service. 
-		Once you have your API keys, the <a href="https://github.com/tokenly/tokenpass-client" target="_blank">TokenpassClient</a>
-		PHP class can be used to integrate into your application. 
-	</p>
-	<p>
-		<strong><a href="http://apidocs.tokenly.com/tokenpass/" target="_blank">View API Documentation</a></strong>
-	</p>
-	<hr>
+	<div class="panel with-padding">
+		@if(Session::has('message'))
+			<p class="alert {{ Session::get('message-class') }}">{{ Session::get('message') }}</p>
+		@endif	
+		
+		<p>
+			Here you can register new client Applications and obtain a pair of API keys for integration of Tokenpass
+			in your own website or service. 
+			Once you have your API keys, the <a href="https://github.com/tokenly/tokenpass-client" target="_blank">TokenpassClient</a>
+			PHP class can be used to integrate into your application. 
+		</p>
+		<p>
+			<strong><a href="http://apidocs.tokenly.com/tokenpass/" target="_blank">View API Documentation</a></strong>
+		</p>
+	</div>
+	<div class="panel with-padding">
+		<table class="table" v-cloak>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th># Users</th>
+					<th>Register Date</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="app in apps">
+					<td><strong>@{{ app.name }}</strong></td>
+					<td>@{{ app.user_count }}</td>
+					<td>@{{ formatDate(app.created_at) }}</td>
+					<td>
+						<button class="reveal-modal" data-modal="viewAppModal" v-on:click="setCurrentApp(app)" ><i class="material-icons">open_in_browser</i> Keys</button>
+					
+						<button class="reveal-modal" data-modal="editAppModal" v-on:click="setCurrentApp(app)" ><i class="material-icons">edit</i> Edit</button>
 
-	<table class="table" v-cloak>
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th># Users</th>
-				<th>Register Date</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr v-for="app in apps">
-				<td><strong>@{{ app.name }}</strong></td>
-				<td>@{{ app.user_count }}</td>
-				<td>@{{ formatDate(app.created_at) }}</td>
-				<td>
-					<button class="reveal-modal" data-modal="viewAppModal" v-on:click="setCurrentApp(app)" ><i class="material-icons">open_in_browser</i> Keys</button>
-				
-					<button class="reveal-modal" data-modal="editAppModal" v-on:click="setCurrentApp(app)" ><i class="material-icons">edit</i> Edit</button>
-
-					<a href="/auth/apps/@{{ app.id }}/delete" onclick="return confirm('Are you sure you want to delete this API key?')"><i class="material-icons">delete</i> Delete</a>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-
+						<a href="/auth/apps/@{{ app.id }}/delete" onclick="return confirm('Are you sure you want to delete this API key?')"><i class="material-icons">delete</i> Delete</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 	<!-- NEW APP MODAL -->
 	<div class="modal-container" id="addAppModal">
 		<div class="modal-bg"></div>
--- resources/views/auth/update.blade.php
+++ resources/views/auth/update.blade.php
@@ -10,50 +10,51 @@
     <span class="heading">Account Settings</span>
 </section>
 
-<section class="panels-container">
-    <div class="panels">
-        <div class="panel">
-            <div class="panel-heading">
-                Update Account
-            </div>
-            <div class="panel-content">
-                @include('partials.errors', ['errors' => $errors])
+<section>
+    <div class="panel with-padding">
+        <div class="panel-heading">
+            Update Account
+        </div>
+        <div class="panel-content">
+            @include('partials.errors', ['errors' => $errors])
 
-                @if(Session::has('message'))
-                    <p class="alert {{ Session::get('message-class') }}">{{ Session::get('message') }}</p>
-                @endif	
+            @if(Session::has('message'))
+                <p class="alert {{ Session::get('message-class') }}">{{ Session::get('message') }}</p>
+            @endif	
 
-                <form method="POST" action="/auth/update">
+            <form method="POST" action="/auth/update">
 
-                    {!! csrf_field() !!}
+                {!! csrf_field() !!}
 
-                    <label for="Name">Name</label>
-                    <input name="name" type="text" id="Name" placeholder="Satoshi Nakamoto" value="{{ old('name') }}">
+                <label for="Name">Name</label>
+                <input name="name" type="text" id="Name" placeholder="Satoshi Nakamoto" value="{{ old('name') }}">
 
-                    <label for="Name">Username</label>
-                    <input disabled value="{{ $model['username'] }}">
+                <label for="Name">Username</label>
+                <input value="{{ $model['username'] }}" readonly>
 
-                    <label for="Email">Email address</label>
-                    <input required="required" name="email" type="email" id="Email" placeholder="youremail@yourwebsite.com" value="{{ old('email') }}">
+                <label for="Email">Email address</label>
+                <input required="required" name="email" type="email" id="Email" placeholder="youremail@yourwebsite.com" value="{{ old('email') }}">
 
-                    <label for="Password">New Password</label>
-                    <div class="sublabel">Enter a new password only if you wish to update your password</div>
-                    <input type="password" id="Password" name="new_password">
+                <label for="Password">New Password</label>
+                <div class="sublabel">Enter a new password only if you wish to update your password</div>
+                <input type="password" id="Password" name="new_password">
 
-                    <label for="Password">Confirm New Password</label>
-                    <input type="password" id="Password" name="new_password_confirmation">
+                <label for="Password">Confirm New Password</label>
+                <input type="password" id="Password" name="new_password_confirmation">
 
-                    <label for="Password">Current Password</label>
-                    <div class="sublabel">Please verify your current password to save your changes</div>
-                    <input required="required" type="password" id="Password" name="password">
+                <label for="Password">Current Password</label>
+                <div class="sublabel">Please verify your current password to save your changes</div>
+                <input required="required" type="password" id="Password" name="password">
 
-                    <label for="account-second-factor">Enable Second Factor on account?</label>
+                <div class="input-group">
+                    <label>Enable Second Factor on account?</label>
                     <input id="account-second-factor" name="second_factor" type="checkbox" class="toggle toggle-round-flat">
+                    <label for="account-second-factor"></label>
+                </div>
+                
+                <button type="submit">Save</button>
 
-                    <button type="submit">Save</button>
-
-                </form>
-            </div>
+            </form>
         </div>
     </div>
 </section>
