4ef727a2b11b5b092d3be76a7217b11a4191cdcf
Devon Weller <devon@tokenly.co>
2016-06-21 21:00:11 -0500

    Updated deploy script to match current deploy process.

--- .deploy.sh
+++ .deploy.sh
@@ -9,10 +9,5 @@ echo; echo "copying sample environment"
 if [ ! -f .env ]; then cp -v .env.example .env; else echo ".env file already exists. Will not overwrite."; fi
 
 echo; echo "compiling assets"
-DIR=`pwd`
-cd resources/assets;
 npm install
-
-
 ./node_modules/gulp/bin/gulp.js
-cd $DIR
