6a4bb2940ec1303ca03d00bfc48139619916b9cb
cryptonaut <nrathman@ironcladtech.ca>
2016-06-28 14:01:16 -0700

    made app link open in new tab

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 			@include('partials.errors', ['errors' => $errors])
 
 			@if(Session::get('url.intended') !== NULL)
-				<div><p class="alert-info">You are about to sign into <strong><a href={{\TKAccounts\Models\OAuthClient::getOAuthClientDetails(Session::get("url.intended"))["app_link"]}}>{{\TKAccounts\Models\OAuthClient::getOAuthClientDetails(Session::get('url.intended'))['name']}}!</a></strong></p>
+				<div><p class="alert-info">You are about to sign into <strong><a href="{{\TKAccounts\Models\OAuthClient::getOAuthClientDetails(Session::get("url.intended"))["app_link"]}}" target="_blank">{{\TKAccounts\Models\OAuthClient::getOAuthClientDetails(Session::get('url.intended'))['name']}}</a></strong></p>
 				</div>
 			@endif
 			<form method="POST" action="/auth/login">
